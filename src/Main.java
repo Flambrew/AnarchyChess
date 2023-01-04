f03e3ef8a863ccd5e5544720dabc03fffd45ba97
Flambrew <andrew.matherne24@bishopireton.org>
2023-01-04 14:02:48 -0500

    project description

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+/**
+ * Anarchy Chess
+ * Chess, with extra rules implemented from the r/AnarchyChess subreddit
+ * 
+ * @author Andrew Matherne
+ * @version 1.0
+ * @since 01.04.2023
+ */
+
+ public class Main {
+
+    public static void main(String[] args) {
+        
+    }
+
+ }
