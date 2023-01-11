18f6188424237a4f386a1661da0486dca9b477e1
Flambrew <77941661+Flambrew@users.noreply.github.com>
2023-01-11 01:18:50 -0500

    cuh

--- src/game/Rules.java
+++ src/game/Rules.java
@@ -1,94 +1,52 @@
 package src.game;
 
-import src.piece.attributes.Color;
-import src.piece.attributes.Column;
-import src.piece.attributes.Row;
-import src.piece.attributes.Type;
+import java.util.ArrayList;
+
+import src.piece.Move;
+import src.piece.Piece;
 
 /**
  * @author Andrew Matherne
  */
 public class Rules {
 
-    private final boolean anarchy;
+    private boolean anarchy = false;
 
-    public Rules(boolean anarchy) {
-        this.anarchy = anarchy;
+    public boolean anarchy(boolean... state) {
+        if (state == null || state.length != 1)
+            return anarchy;
+        return anarchy = state[0];
     }
 
-    private boolean getLegalMoves(Board board) {
-
+    public ArrayList<Move> getLegalMoves(ArrayList<Piece> board) {
+        ArrayList<Move> moves = new ArrayList<Move>();
+        for (Piece piece : board)
+            moves.addAll(getLegalMoves(piece));
+        return moves;
     }
 
-    // TODO:rules-B,R,K
+    private ArrayList<Move> getLegalMoves(Piece piece) {
+        int row = piece.row().num;
+        int column = piece.column().num;
 
-    private boolean evalMove(int from, int to, int... validMoves) {
-        for (int validMove : validMoves)
-            if (to - from == validMove)
-                return true;
-        return false;
-    }
-
-    // alg - a=(from%8+from/8);b=(to%8+to/8);a%2==b%2&&(a%7==b%7||a%9==b%9)
-    private boolean evalSlide(int from, int to, Axis axis) {
-        if (board.getPiece(from).color() == board.getPiece(to).color())
-            return false;
-        if (axis == Axis.CARDINAL) {
-            if (from / 8 == to / 8) {
-                
-            } else if (from % 8 == to % 8) {
-
-            }
-        } else if (axis == Axis.DIAGONAL) {
-            if ((from / 8 + from % 8) % 2 == (to / 8 + to % 8) % 2 && (from % 7 == to % 7 || from % 9 == to % 9)) {
-
-            }
+        switch (piece.type()) {
+            case BISHOP:
+                break;
+            case KING:
+                break;
+            case KNIGHT:
+                break;
+            case KNOOK:
+                break;
+            case PAWN:
+                break;
+            case QUEEN:
+                break;
+            case ROOK:
+                break;
+            default:
+                break;
         }
-        return false;
-    }
-
-    private boolean pawn(int from, int to) {
-        // TODO: Pawn: En Passant
-        if (board.getPiece(from).color() == board.getPiece(to).color())
-            return false;
-        if (board.getPiece(from).color() == Color.WHITE) {
-            if (board.getPiece(to).color() == Color.NONE) {
-                if (from / 8 == 6 && board.getPiece(from - 8).type() == Type.NONE)
-                    return evalMove(from, to, -8, -16);
-                return evalMove(from, to, -8);
-            } else {
-                return evalMove(from, to, -9, -7);
-            }
-        } else {
-            if (board.getPiece(to).color() == Color.NONE) {
-                if (from / 8 == 6 && board.getPiece(from + 8).type() == Type.NONE)
-                    return evalMove(from, to, 8, 16);
-                return evalMove(from, to, 8);
-            } else {
-                return evalMove(from, to, 7, 9);
-            }
-        }
-    }
-
-    private boolean knight(int from, int to) {
-        if (board.getPiece(from).color() == board.getPiece(to).color())
-            return false;
-        return evalMove(from, to, -17, -15, -10, -8, 8, 10, 15, 17);
-    }
-
-    private boolean bishop(int from, int to) {
-        return false;
-    }
-
-    private boolean rook(int from, int to) {
-        return false;
-    }
-
-    private boolean queen(int from, int to) {
-        return rook(from, to) || bishop(from, to);
-    }
-
-    private boolean king(int from, int to) {
-        return false;
+        return null;
     }
 }
