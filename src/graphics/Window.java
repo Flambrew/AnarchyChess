fff97cd4daf09204cf0ea7223cd907d987308e27
Flambrew <77941661+Flambrew@users.noreply.github.com>
2023-01-13 00:32:59 -0500

    javafx aint lookin much sweeter

--- src/graphics/Window.java
+++ src/graphics/Window.java
@@ -1,57 +1,16 @@
 package src.graphics;
 
-import javax.swing.JFrame;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-import java.awt.Color;
-//import java.awt.event.ComponentEvent;
-//import java.awt.event.ComponentAdapter;
+public class Window extends Application {
 
-import src.game.constructs.Vector2;
-
-public class Window {
-
-    private JFrame frame;
-    //private Drawing drawing;
-
-    private Vector2 windowSize;
-
-    private boolean exitsOnClose;
-
-    public Window() {
-        this.frame = new JFrame("Window");
-        //this.drawing = new Drawing();
-        this.windowSize = new Vector2(1280, 720);
-        this.exitsOnClose = true;
-    }
-
-    public Window(String name, Vector2 windowSize, boolean exitsOnClose) {
-        this.frame = new JFrame(name);
-        //this.drawing = new Drawing();
-        this.windowSize = new Vector2(windowSize.X, windowSize.Y);
-        this.exitsOnClose = exitsOnClose;
-    }
-
-    public boolean exitsOnClose() {
-        return exitsOnClose;
-    }
-
-    public void setExitsOnClose(boolean exitsOnClose) {
-        this.exitsOnClose = exitsOnClose;
-    }
-
-    public void init() {
-        frame.setDefaultCloseOperation(exitsOnClose ? 3 : 1);
-        frame.setSize(windowSize.X, windowSize.Y);
-        frame.setLocationRelativeTo(null);
-        frame.setBackground(Color.RED);
-        frame.setVisible(true);
-        frame.toFront();
-        //frame.add(drawing);
-
-        /* frame.addComponentListener(new ComponentAdapter() {
-            public void componentResized(ComponentEvent e) {
-
-            }
-        });*/
+    public void start(Stage mainStage) {
+        mainStage.setTitle("My Window");
+        mainStage.setWidth(1280);
+        mainStage.setHeight(720);
+        mainStage.setResizable(false);
+        mainStage.show();
+        
     }
 }
--- src/main/Testing.java
+++ src/main/Testing.java
@@ -1,13 +1,10 @@
 package src.main;
 
-import src.game.constructs.Vector2;
+import javafx.application.Application;
 import src.graphics.Window;
 
 public class Testing {
-
-    private static Window window;
     public static void main(String[] args) {
-        window = new Window("AnarchyChess", new Vector2(1280, 720), true);
-        window.init();
+        Application.launch(Window.class, args);
     }
 }
